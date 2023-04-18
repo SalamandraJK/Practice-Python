6019d1fde67dea4eb5d9d117d4fb0a6fa649d6fe
MikhailPL <muke.jry@gmail.com>
2023-04-18 22:33:13 +0300

    practice

--- Practice01.py
+++ Practice01.py
@@ -113,18 +113,116 @@
 
 # Пример работы со строками и текстом
 
-text = 'OOOPS! Yes, I did iT agaIn'
-print(len(text)) # Функция len, позволяет узнать длинну строки
-print('Yes'in text) # Проверка на наличии строки в тексте
-print(text.lower()) # Перевод всех символом в нижний регистр
-print(text.upper()) # Перевод всех символом в верхний регистр
-print(text.replace('Yes', 'YES')) # Позволяет поменять сочетание символов в нашей строке
+# text = 'OOOPS! Yes, I did iT agaIn'
+# print(len(text)) # Функция len, позволяет узнать длинну строки
+# print('Yes'in text) # Проверка на наличии строки в тексте
+# print(text.lower()) # Перевод всех символом в нижний регистр
+# print(text.upper()) # Перевод всех символом в верхний регистр
+# print(text.replace('Yes', 'YES')) # Позволяет поменять сочетание символов в нашей строке
 
 
-text = 'съешь ещё этих мягких французских булок'
-print(len(text)) # Функция len, позволяет узнать длинну строки
-print(text[:20])
-print(text[0:len(text):6])  
+# text = 'съешь ещё этих мягких французских булок'
+# print(len(text)) # Функция len, позволяет узнать длинну строки
+# print(text[:20])
+# print(text[0:len(text):6])  
 
-text = text[2:9] + text[-5] + text[:2]
-print(text)
+# text = text[2:9] + text[-5] + text[:2]
+# print(text)
+
+
+# list_1 = [7, 9, 11, 13, 15, 17]
+# print(list_1[0])
+
+# for i in list_1:        # Вывод всех элементо  списка
+#     print (i)
+
+# print(len(list_1))      # Длинна списка
+
+# list_1 = [7, 9, 11, 13, 15, 17]
+# list_1.append(77)
+# print(list_1)
+
+# list_1 = []
+# for i in range(5):
+#     list_1.append(i)
+#     print(list_1)
+
+# t = ()
+# print(type(t))
+
+# t = (1, 2, 3,)
+# print(type(t))
+
+# v = [1, 3, 5]
+# print(v)
+# print(type(v))
+
+# v = type(v)
+# print(v)
+# print(type(v))
+
+
+# a,b,c = v
+
+# print(a, b, c)
+
+
+# t = (1, 2, 3, 4, 5,)
+
+# print(t[1])
+
+# for i in t:
+#     print(i)
+
+# for i in range(len(t)):
+#     print(t[i])
+
+
+# c = [1, 2, 3, 4, 5,]
+
+# c[0] = 2
+
+# print(c)
+# d = {}
+# d['q'] = 'qwerty'
+# print(d)
+# d['w'] = 'werty'
+# print(d)
+
+# print(d['q'])
+
+# diction = {'ffff' : 'f', 'l' : '3', 'r' : '4'}
+# for item in diction:
+#     print('{} : {}'.format(item, diction[item]))
+
+# print(diction.items())
+
+
+# colors = {'red', 'green', 'blue'}
+# print(colors)
+# colors.add('darck')
+# print(colors)
+# colors.remove('darck')
+# print(colors)
+# colors.discard('darck')
+# print(colors)
+# colors.clear()
+# print(colors)
+
+# q = set()
+
+a = {1, 2, 3, 5, 7}
+b = {2, 5, 6, 8, 12, 13}
+c = a.copy()    #Копировать значения
+print(c)
+u = a.union(b)  #Будет содержать уникальные значения обоих множеств
+print(u)
+i = a.intersection(b)   # Будет содержать общие элементы из двух списков
+print(i)
+dl = a.difference(b)    # Будет содержать элементы вычтенные множеством А от множества В
+print(dl)
+dr = b.difference(a)    # Будет содержать элементы вычтенные множеством В от множества А
+print(dr)
+q = a.union(b).difference(a.intersection(b))    #    
+print(q)
+
